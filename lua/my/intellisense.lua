7bd3936cffa508778131182b2cd8ced676ad9704
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2025-06-20 23:09:00 -0400

    Fix nixd

--- lua/my/intellisense.lua
+++ lua/my/intellisense.lua
@@ -49,14 +49,26 @@ M.config = {
 		---@type vim.lsp.ClientConfig[]
 		servers = {
 			nixd = {
+				enabled = true,
 				cmd = { "nixd", "--semantic-tokens=true" },
 				settings = {
 					nixd = {
 						nixpkgs = {
-							expr = "import nixpkgs { }",
+							expr = "import <nixpkgs> { }",
 						},
 						formatting = {
 							command = { "nixfmt" },
+						},
+						options = {
+							["nix-darwin"] = {
+								expr = [[(builtins.getFlake  ("git+file://" + toString ./.)).darwinConfigurations.Isaacs-MacBook-Pro.options]],
+							},
+							nixos = {
+								expr = [[(builtins.getFlake  ("git+file://" + toString ./.)).nixosConfigurations.Isaac-NixOS.options]],
+							},
+							["home-manager"] = {
+								expr = '(builtins.getFlake  ("git+file://" + toString ./.)).darwinConfigurations.Isaacs-MacBook-Pro.options.home-manager.users.type.getSubOptions []',
+							},
 						},
 					},
 				},
